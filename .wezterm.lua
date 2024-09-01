747a8e178c2701de3c039da506ce26910e7244aa
Michal Olechowski <michalolechowskidev@gmail..com>
2024-09-01 10:23:57 +0200

    Clean comments in .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
@@ -13,8 +9,6 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-
--- my coolnight colorscheme:
 config.colors = {
 	foreground = "#CBE0F0",
 	background = "#011423",
@@ -30,5 +24,4 @@ config.colors = {
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
--- and finally, return the configuration to wezterm
 return config
